b35c7a865496d58c7a5c6f1b4ba9284522da5ca5
Erik <erik.ra@gmail.com>
2017-04-26 01:25:18 +0200

    Add project files.

---
+++ CryptographersConundrum/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptographersConundrum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int days = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    if (input[i] != 'P')
+                    {
+                        days++;
+                    }
+                }
+                else if (i % 3 == 1)
+                {
+                    if (input[i] != 'E')
+                    {
+                        days++;
+                    }
+                }
+                else if (i % 3 == 2)
+                {
+                    if (input[i] != 'R')
+                    {
+                        days++;
+                    }
+                }
+            }
+
+            Console.WriteLine(days);
+        }
+    }
+}
